88a5654c4300c0db82e13e0871da343323504ef4
Kirill K <kovalev.kirill.a@gmail.com>
2022-09-23 10:05:10 +0300

    chore: save progress

---
+++ frontend/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Header from './lib/Header.svelte'
+  import Panel from './lib/Panel.svelte'
+  import Workspace from './lib/Workspace.svelte'
+</script>
+
+<div class="columns">
+  <div class="column" id="header">
+    <Header />
+  </div>
+</div>
+
+<div class="columns">
+  <div class="column is-one-fifth" id="panel">
+    <Panel />
+  </div>
+
+  <div class="column" id="workspace">
+    <Workspace />
+  </div>
+</div>
---
+++ frontend/src/lib/Panel.svelte
@@ -0,0 +1,18 @@
+<nav>
+  <h1>Panel</h1>
+
+  <button class="button is-link is-fullwidth">
+    <span class="icon is-medium">
+      <i class="fa fa-github"></i>
+    </span>
+    <span>Teams</span>
+  </button>
+
+  <button class="button is-link is-fullwidth is-inverted">
+    Services
+  </button>
+
+  <button class="button is-link is-fullwidth is-inverted">
+    Info
+  </button>
+</nav>
