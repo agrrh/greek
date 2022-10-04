173797d5a6874a2fd74f58ab0586d0cc6480ce17
Kirill K <kovalev.kirill.a@gmail.com>
2022-10-04 23:15:14 +0300

    chore: save progress on team page

--- frontend/src/lib/team/TableRow.svelte
+++ frontend/src/lib/team/TableRow.svelte
@@ -8,11 +8,41 @@
     </a>
   </th>
   <td>
-    <button is-small class="button is-small is-info">version: 0.0.1</button>
-    <button is-small class="button is-small is-info">tech: python</button>
-    <button is-small class="button is-small is-success">uptime: 99.95%</button>
-    <button is-small class="button is-small is-danger">review: failed</button>
-    <button is-small class="button is-small is-success">sla: ok</button>
-    <button is-small class="button is-small is-warning">backup: outdated</button>
+    <button is-small class="button is-small is-info">
+      <span class="icon">
+        <i class="fa fa-box"></i>
+      </span>
+      <span>0.0.1</span>
+    </button>
+    <button is-small class="button is-small is-info">
+      <span class="icon">
+        <i class="fa fa-gears"></i>
+      </span>
+      <span>python</span>
+    </button>
+    <button is-small class="button is-small is-success">
+      <span class="icon">
+        <i class="fa fa-circle-check"></i>
+      </span>
+      <span>99.95%</span>
+    </button>
+    <button is-small class="button is-small is-warning">
+      <span class="icon">
+        <i class="fa fa-code-pull-request"></i>
+      </span>
+      <span>2</span>
+    </button>
+    <button is-small class="button is-small is-success">
+      <span class="icon">
+        <i class="fa fa-file-waveform"></i>
+      </span>
+      <span>ok</span>
+    </button>
+    <button is-small class="button is-small is-danger">
+      <span class="icon">
+        <i class="fa fa-file-zipper"></i>
+      </span>
+      <span>outdated</span>
+    </button>
   </td>
 </tr>
